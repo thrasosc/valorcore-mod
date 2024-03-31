21cf96c8a639e3f77e2b9d2a4020d678e4bbe319
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-03-31 20:21:12 +0200

    add guide book button in inventory screen

---
+++ src/main/java/net/pixeldreamstudios/valorcore/ValorCoreClient.java
@@ -0,0 +1,25 @@
+package net.pixeldreamstudios.valorcore;
+
+import io.wispforest.lavender.book.BookLoader;
+import io.wispforest.lavender.client.LavenderBookScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.IconButtonWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ValorCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof InventoryScreen) {
+//                Screens.getButtons(screen).add(IconButtonWidget.builder(Text.empty(), new Identifier("minecraft", "item/gold_nugget"), button -> client.setScreen(new LavenderBookScreen(BookLoader.get(new Identifier(ValorCore.MOD_ID, "valor_guide"))))).iconSize(16,16).textureSize(16, 16)..build());
+                Screens.getButtons(screen).add(ButtonWidget.builder(Text.literal("Guide book"), p -> client.setScreen(new LavenderBookScreen(BookLoader.get(new Identifier(ValorCore.MOD_ID, "valor_guide"))))).position(screen.width / 2 + 7, screen.height / 5 + 3).size(70, 14).build());
+            }
+        });
+    }
+}
