7da925ed871321a397d6b16bfc3053b034c33c67
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-04-03 12:56:06 +0200

    add spring icon item

--- src/main/java/net/pixeldreamstudios/registry/ItemRegistry.java
+++ src/main/java/net/pixeldreamstudios/registry/ItemRegistry.java
@@ -3,11 +3,18 @@ package net.pixeldreamstudios.registry;
 import io.wispforest.lavender.book.LavenderBookItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.pixeldreamstudios.valorcore.ValorCore;
 
 public class ItemRegistry {
     public static final Item VALORCORE_ALMANAC = LavenderBookItem.registerForBook(new Identifier(ValorCore.MOD_ID, "valor_guide"), new Identifier(ValorCore.MOD_ID, "valorcore_valor_guide"), new FabricItemSettings().maxCount(1));
+    public static final Item SPRING_ICON = registerItem("spring_icon", new Item(new FabricItemSettings().maxCount(1)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ValorCore.MOD_ID, name), item);
+    }
 
     public static void init() {
         ValorCore.LOGGER.info("Registering items");
