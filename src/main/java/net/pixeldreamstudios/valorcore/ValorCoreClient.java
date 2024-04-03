0d736420ef1af0e594adf29937647544187fed8f
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-04-03 17:44:47 +0200

    specify client side in client initialiser

--- src/main/java/net/pixeldreamstudios/valorcore/ValorCoreClient.java
+++ src/main/java/net/pixeldreamstudios/valorcore/ValorCoreClient.java
@@ -1,8 +1,11 @@
 package net.pixeldreamstudios.valorcore;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.pixeldreamstudios.valorcore.util.ClientStuff;
 
+@Environment(EnvType.CLIENT)
 public class ValorCoreClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
