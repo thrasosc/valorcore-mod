e144795db720b2095c12cf1541721c57e1f81f48
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-06-01 01:00:28 +0200

    remove craft book from crafting screen

--- src/main/java/net/pixeldreamstudios/valorcore/util/ClientStuff.java
+++ src/main/java/net/pixeldreamstudios/valorcore/util/ClientStuff.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.ingame.CraftingScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
@@ -35,6 +36,22 @@ public class ClientStuff {
                     }
                 }
                 Screens.getButtons(screen).add(ButtonWidget.builder(Text.literal("Guide Book"), p -> client.setScreen(new LavenderBookScreen(BookLoader.get(new Identifier(ValorCore.MOD_ID, "valor_guide"))))).position(scaledWidth / 2 + 8, scaledHeight / 2 - 98).size(80, 16).build());
+            }
+            else if (screen instanceof CraftingScreen) {
+                List<? extends Element> widgets = screen.children();
+                for (Element widget : widgets) {
+                    if (widget instanceof TexturedButtonWidget) {
+                        try {
+                            TexturedButtonWidget texturedButtonWidget = (TexturedButtonWidget) widget;
+                            if (texturedButtonWidget.getWidth() == 20 && texturedButtonWidget.getHeight() == 18) {
+                                texturedButtonWidget.active = false;
+                                texturedButtonWidget.visible = false;
+                            }
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                }
             }
         });
     }
